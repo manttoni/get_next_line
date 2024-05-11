f69031f4b46f6205e72b5c531e6ae2304ecbe590
Anttoni Maula <amaula@c2r2p3.hive.fi>
2024-05-11 13:11:41 +0300

    norminette

--- get_next_line.c
+++ get_next_line.c
@@ -1,40 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amaula <amaula@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/11 12:40:51 by amaula            #+#    #+#             */
+/*   Updated: 2024/05/11 12:42:04 by amaula           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
+
+static int	init(char **next_line, char **nl_chr, char *buffer, int fd)
+{
+	if (fd < 0)
+		return (0);
+	*next_line = malloc(1);
+	if (*next_line == NULL)
+		return (0);
+	**next_line = '\0';
+	*nl_chr = ft_strchr(buffer, '\n');
+	return (1);
+}
+
+static int	read_to_buffer(int fd, char *buffer, char **next_line)
+{
+	ssize_t	bytes_read;
+
+	ft_bzero(buffer, BUFFER_SIZE + 1);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read < 0)
+	{
+		free(*next_line);
+		return (-1);
+	}
+	if (bytes_read == 0)
+	{
+		if (**next_line == '\0')
+		{
+			free(*next_line);
+			return (-1);
+		}
+		return (0);
+	}
+	return (1);
+}
 
 char	*get_next_line(int fd)
 {
 	static char	buffer[BUFFER_SIZE + 1];
 	char		*next_line;
 	char		*nl_chr;
-	ssize_t		bytes_read;
+	int			read_result;
 
-	if (fd < 0)
+	if (init(&next_line, &nl_chr, buffer, fd) == 0)
 		return (NULL);
-	next_line = malloc(1);
-	if (!next_line)
-		return (NULL);
-	*next_line = '\0';
-	nl_chr = ft_strchr(buffer, '\n');
 	while (nl_chr == NULL)
 	{
 		next_line = gnl_join(next_line, buffer, ft_strlen(buffer));
 		if (!next_line)
 			return (NULL);
-		ft_bzero(buffer, BUFFER_SIZE + 1);
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_read < 0)
-		{
-			free(next_line);
+		read_result = read_to_buffer(fd, buffer, &next_line);
+		if (read_result == -1)
 			return (NULL);
-		}
-		if (bytes_read == 0)
-		{
-			if (*next_line == '\0')
-			{
-				free(next_line);
-				return (NULL);
-			}
+		if (read_result == 0)
 			return (next_line);
-		}
 		nl_chr = ft_strchr(buffer, '\n');
 	}
 	next_line = gnl_join(next_line, buffer, (nl_chr - buffer) + 1);
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amaula <amaula@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/11 12:41:59 by amaula            #+#    #+#             */
+/*   Updated: 2024/05/11 12:42:01 by amaula           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 size_t	ft_strlen(char *s)
@@ -14,9 +26,9 @@ size_t	ft_strlen(char *s)
 
 char	*gnl_join(char *dst, char *src, size_t len)
 {
-	char		*ret;
-	char		*ptr;
-	size_t		bytes;
+	char			*ret;
+	char			*ptr;
+	size_t			bytes;
 	unsigned int	i;
 
 	if (!*src)
